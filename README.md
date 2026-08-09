d78d79656963a9e09b09a8de99da1654ecbc15f1
davooojkk <davyhenn11@gmail.com>
2026-08-09 10:38:24 -0300

    Drag & Drop, eliminacion de botones y ajustes css

---
+++ README.md
@@ -0,0 +1,20 @@
+# kanbandavoootech
+
+-- versión 1.0 --
+No considero que sea necesario ponerme a explicar para que sirve un Kan Ban
+pero en el caso de que no lo conozcas, a continuación lo explico brevemente.
+
+El Kan Ban es un sistema visual desarrollado en los años '40 en Toyota, Japón.
+El Ingeniero Taiichi Ohno creo este sistema para organizar stocks y tareas de
+la mejor forma posible.
+
+Esta primera versión cuenta con funciones básicas como añadir tareas, editarlas,
+cambiar su estado de manera progresiva y eliminarlas. Nada más. Un sistema simple
+y practico, en proximas versiones pretendo añadir nuevas funciones.
+
+-- versión 1.1 --
+
+En esta versión, podemos disfrutar de un comodo drag & drop, eliminé el botón de avanzar la tarea ya que queda
+completamente inutilizado, ajusté el css eliminando los
+estilos que ya no se usan, cambié el grotesco boton de
+eliminar por un simple emoji minimalista.
---
+++ js/script.js
@@ -0,0 +1,193 @@
+// ---- selectores DOM ---- //
+
+const formulario = document.querySelector("#task-form");
+const formularioInput = document.querySelector("#task-input");
+const sectionPendientes = document.querySelector("#pendientes");
+const sectionProgresos = document.querySelector("#en-progreso");
+const sectionFinalizados = document.querySelector("#finalizados");
+
+// ---- verificamos tareas guardadas ---- //
+
+const tareasGuardadas = window.localStorage.getItem("Mis Tareas");
+
+let tareas;
+
+if (tareasGuardadas !== null) {
+  tareas = JSON.parse(tareasGuardadas);
+} else {
+  tareas = [];
+}
+
+// ---- evento del formulario ---- //
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombreTarea = formularioInput.value.trim();
+
+  if (nombreTarea === "") {
+    return;
+  }
+
+  crearTarea(nombreTarea);
+
+  formularioInput.value = "";
+});
+
+// ---- function crear tarea ---- //
+
+function crearTarea(nombre) {
+  const nuevaTarea = {
+    id: Date.now(),
+    nombre: nombre,
+    estado: "pendiente",
+  };
+
+  tareas.push(nuevaTarea);
+
+  guardarTareas();
+  renderTareas();
+}
+
+// ---- function eliminar tareas ---- //
+
+function eliminarTarea(id) {
+  const tareaEncontrada = tareas.findIndex((tarea) => tarea.id === id);
+
+  if (tareaEncontrada !== -1) {
+    tareas.splice(tareaEncontrada, 1);
+
+    guardarTareas();
+    renderTareas();
+  }
+}
+
+// ---- cambiar estado ---- //
+
+function cambiarEstado(id, nuevoEstado) {
+  const tareaEncontrada = tareas.find((tarea) => tarea.id === id);
+
+  if (tareaEncontrada !== undefined) {
+    tareaEncontrada.estado = nuevoEstado;
+
+    guardarTareas();
+    renderTareas();
+  }
+}
+
+// ---- creación de tarjeta ---- //
+
+function crearTarjeta(tarea) {
+  const tarjetaDiv = document.createElement("div");
+  tarjetaDiv.classList.add("tarea");
+  activarDrag(tarea, tarjetaDiv);
+  const tarjetaTitulo = document.createElement("h1");
+  tarjetaTitulo.innerText = tarea.nombre;
+  activarEdicion(tarea, tarjetaTitulo);
+  const botonEliminar = document.createElement("button");
+  botonEliminar.innerText = "🗑️";
+
+  botonEliminar.addEventListener("click", () => {
+    eliminarTarea(tarea.id);
+  });
+
+  tarjetaDiv.append(tarjetaTitulo, botonEliminar);
+
+  return tarjetaDiv;
+}
+
+// ---- cambiar nombre de las tareas ---- //
+
+function editarNombre(id, nuevoNombre) {
+  const tareaEncontrada = tareas.find((tarea) => tarea.id === id);
+
+  if (tareaEncontrada !== undefined) {
+    tareaEncontrada.nombre = nuevoNombre;
+
+    guardarTareas();
+    renderTareas();
+  }
+}
+
+// ---- abrir input de edicion de nombre ---- //
+
+function activarEdicion(tarea, tarjetaTitulo) {
+  tarjetaTitulo.addEventListener("click", () => {
+    const inputEditar = document.createElement("input");
+
+    inputEditar.classList.add("input-edit-nombres");
+    inputEditar.value = tarea.nombre;
+
+    tarjetaTitulo.replaceWith(inputEditar);
+    inputEditar.focus();
+
+    function guardarEdicion() {
+      const nuevoNombre = inputEditar.value.trim();
+
+      if (nuevoNombre !== "") {
+        editarNombre(tarea.id, nuevoNombre);
+      }
+    }
+
+    inputEditar.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        guardarEdicion();
+      }
+    });
+
+    inputEditar.addEventListener("blur", guardarEdicion);
+  });
+}
+
+// ---- render tareas ---- //
+
+function renderTareas() {
+  sectionPendientes.innerHTML = "";
+  sectionProgresos.innerHTML = "";
+  sectionFinalizados.innerHTML = "";
+
+  tareas.forEach((tarea) => {
+    const tarjeta = crearTarjeta(tarea);
+
+    if (tarea.estado === "pendiente") {
+      sectionPendientes.appendChild(tarjeta);
+    } else if (tarea.estado === "en progreso") {
+      sectionProgresos.appendChild(tarjeta);
+    } else if (tarea.estado === "finalizado") {
+      sectionFinalizados.appendChild(tarjeta);
+    }
+  });
+}
+
+// ---- guardar tareas ---- //
+
+function guardarTareas() {
+  localStorage.setItem("Mis Tareas", JSON.stringify(tareas));
+}
+
+renderTareas();
+
+// ---- Drag & Drop ---- //
+
+function activarDrag(tarea, tarjetaDiv) {
+  tarjetaDiv.draggable = true;
+
+  tarjetaDiv.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("text/plain", tarea.id);
+  });
+}
+
+function activarDrop(columna, nuevoEstado) {
+  columna.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
+
+  columna.addEventListener("drop", (event) => {
+    const idTarea = Number(event.dataTransfer.getData("text/plain"));
+    cambiarEstado(idTarea, nuevoEstado);
+  });
+}
+
+activarDrop(sectionPendientes, "pendiente");
+activarDrop(sectionProgresos, "en progreso");
+activarDrop(sectionFinalizados, "finalizado");
